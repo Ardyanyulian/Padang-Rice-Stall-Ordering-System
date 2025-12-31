edf6e532efe588956d00784ea4f906b2419ee17a
Ridang bima pamungkas <ridangbp@gmail.com>
2025-12-31 18:36:07 +0700

    menambahkan bagian profile, pengaturan, gudang, staff,login,manage warung, transaksi, tambah barang, laporan

--- menu_display/urls.py
+++ menu_display/urls.py
@@ -4,7 +4,17 @@ from . import views
 
 urlpatterns = [
     # Ketika alamat utama diakses (URL kosong), panggil views.tampilkan_menu
-    path('', views.tampilkan_index, name='index'),
+    path('', views.tampilkan_login, name='login'),
     path('daftar_menu/', views.tampilkan_menu, name='daftar_menu'),
-    path('about/', views.tampilkan_about , name='about')
+    path('about/', views.tampilkan_about , name='about'),
+    path('dashboard/', views.tampilkan_dashboard , name='dashboard'),
+    path('login/', views.tampilkan_login , name='login'),
+    path('transaksi/', views.tampilkan_transaksi , name='transaksi'),
+    path('profile/', views.tampilkan_profile , name='profile'),
+    path('gudang/', views.tampilkan_gudang , name='gudang'),
+    path('settings/', views.tampilkan_settings , name='settings'),
+    path('staff/', views.tampilkan_staff , name='staff'),
+    path('laporan/', views.tampilkan_laporan , name='laporan'),
+    path('manage/', views.tampilkan_manage , name='manage'),
+    path('tambah/', views.tampilkan_tambah , name='tambah'),
 ]
--- menu_display/views.py
+++ menu_display/views.py
@@ -30,8 +30,39 @@ def tampilkan_index(request):
     }
     
 
-    return render(request, 'menu_display/dasboard.html', context)
+    return render(request, 'menu_display/index.html', context)
 
 
 def tampilkan_about(request):
     return render(request, 'about/about.html')
+
+
+def tampilkan_dashboard(request):
+    return render(request, 'menu_display/dashboard.html')
+
+def tampilkan_login(request):
+    return render(request, 'menu_display/login.html')
+
+def tampilkan_transaksi(request):
+    return render(request, 'menu_display/transaksi.html')
+
+def tampilkan_profile(request):
+    return render(request, 'menu_display/profile.html')
+
+def tampilkan_gudang(request):
+    return render(request, 'menu_display/gudang.html')
+
+def tampilkan_settings(request):
+    return render(request, 'menu_display/settings.html')
+
+def tampilkan_staff(request):
+    return render(request, 'menu_display/staff.html')
+
+def tampilkan_laporan(request):
+    return render(request, 'menu_display/laporan.html')
+
+def tampilkan_manage(request):
+    return render(request, 'menu_display/manage.html' )
+
+def tampilkan_tambah(request):
+    return render(request, 'menu_display/tambah.html' )
